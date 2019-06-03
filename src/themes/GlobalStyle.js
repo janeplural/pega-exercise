e71faf4825cc69e7a539cc9121766033df7f9b2c
janeplural <mlle.jihshin@gmail.com>
2019-06-03 15:11:24 -0400

    switch box-sizing from content to border

--- src/themes/GlobalStyle.js
+++ src/themes/GlobalStyle.js
@@ -2,7 +2,11 @@ import { createGlobalStyle } from 'styled-components'
 
 const GlobalStyle = createGlobalStyle`
   html {
+    box-sizing: border-box;
     font-size: 10px;
+  }
+  *, *:before, *:after {
+    box-sizing: inherit;
   }
   body {
     font-family: 'Open Sans', sans-serif;
