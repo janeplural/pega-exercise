117e89bc457638464ef0bb1b8e34d6e69f87878d
janeplural <mlle.jihshin@gmail.com>
2019-06-03 20:58:07 -0400

    make main element styles global

--- src/components/SundayPage.js
+++ src/components/SundayPage.js
@@ -1,23 +1,14 @@
 import React from 'react';
-import styled from 'styled-components';
 import Hero from './Hero';
 import EventList from './EventList';
 import agenda from '../data/agenda.json';
 
-const Main = styled.main`
-  margin: 0 auto;
-  width: 90vw;
-  max-width: 99.2rem;
-  display: flex;
-  flex-direction: column;
-`
-
 const SundayPage = () => (
   <>
     <Hero heading="Sunday, June 3"/>
-    <Main>
+    <main>
       <EventList data={agenda[0].events}/>
-    </Main>
+    </main>
   </>
 );
 
