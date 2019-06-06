be90a9e4886087de1935cead1e0af29bb0d2ff6a
janeplural <mlle.jihshin@gmail.com>
2019-06-06 12:37:18 -0400

    Updates

---
+++ precache-manifest.22d1796a726ff1c523db616dbd60bd63.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8af33f520139fce77e78a15d94e6eb78",
+    "url": "/pega-exercise/index.html"
+  },
+  {
+    "revision": "e87da15431bad8de821b",
+    "url": "/pega-exercise/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "e87da15431bad8de821b",
+    "url": "/pega-exercise/static/js/2.c5368cb1.chunk.js"
+  },
+  {
+    "revision": "e4cb45ecf6b1b7456c85",
+    "url": "/pega-exercise/static/js/main.807aaadb.chunk.js"
+  },
+  {
+    "revision": "94e02524962fbf6d3a9d",
+    "url": "/pega-exercise/static/js/runtime~main.e9e518b9.js"
+  },
+  {
+    "revision": "8818c2be30c66685b79c841ca577b6fb",
+    "url": "/pega-exercise/static/media/logo-desktop.8818c2be.svg"
+  },
+  {
+    "revision": "86a1e22a4239055b4cbe4b2542df767b",
+    "url": "/pega-exercise/static/media/logo-mobile.86a1e22a.svg"
+  }
+]);
