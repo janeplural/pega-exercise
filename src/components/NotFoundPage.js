4085e479dafddc9f387e0e53c81e8a9bb20efe71
janeplural <mlle.jihshin@gmail.com>
2019-06-06 16:25:05 -0400

    update and styled PageNotFound component

--- src/components/NotFoundPage.js
+++ src/components/NotFoundPage.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import styled from 'styled-components';
 
-const NotFoundPage = () => (
-  <div>
-    <h1>Not Found</h1>    
-  </div>
-);
+const Main = styled.main`
+  padding-top: 10rem;
+`;
+
+class NotFoundPage extends React.Component {
+  componentDidMount() {
+    window.scrollTo(0, 0);
+  }
+
+  render() {
+    return (
+      <Main>
+        <h1>Page could not be located.</h1>
+        <h2>Please ensure the correct URL was used.</h2>
+      </Main>
+    )
+  }
+}
 
 export default NotFoundPage;
